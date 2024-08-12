a067f898d119b47693eeee97f3c3483e271290cb
Kunal Pathak <kunal.pathak@unc.edu>
2024-08-12 15:11:16 -0400

    Updated Website

--- config/config.js
+++ config/config.js
@@ -1,169 +1,150 @@
 
-import profile from './profile.png';
+import profile from './boston_headshot.JPEG';
 import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-  name: "Hashir",
-  links: [
-    {
-      title: "About",
-      link: "#about",
-    },
-    {
-      title: "Projects",
-      link: "#projects",
-    },
-    {
-      title: "Contact",
-      link: "#contact",
-    },
-    {
-      title: "Links",
-      link: "/links",
-    },
-    {
-      title: "Blog",
-      link: "https://medium.com/@hashirshoaeb",
-    }
-  ],
+	name: "Kunal",
+	links: [
+		{
+			title: "About",
+			link: "#about",
+		},
+		{
+			title: "Projects",
+			link: "#projects",
+		},
+		{
+			title: "Contact",
+			link: "#contact",
+		},
+		{
+			title: "Links",
+			link: "/links",
+		},
+	],
 }
 export const intro = {
-  title: "Hey, I'm Hashir",
-  description: "A Computer Engineer creating mobile apps and static websites.",
-  image: profile.src,
-  buttons: [
-    {
-      title: "Contact Me",
-      link: "#contact",
-      isPrimary: true,
-    },
-    {
-      title: "Resume",
-      link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-      isPrimary: false,
-    },
-  ],
+	title: "Hey, I'm Kunal!",
+	description: "A Computer Science graduate student interested in backend engineering.",
+	image: profile.src,
+	buttons: [
+		{
+			title: "Contact Me",
+			link: "#contact",
+			isPrimary: true,
+		},
+		{
+			title: "Resume",
+			link: "https://docs.google.com/document/d/1fRq1apiTb5LCXV2bZxzIvifqu-NH0C606o0Hew-g3Gg/edit?usp=sharing",
+			isPrimary: false,
+		},
+	],
 }
 
 export const about = {
-  title: "Who I am",
-  description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
-  ],
+	title: "Who I am",
+	description: [
+		"I am a Masters student studying Computer Science at UNC-Chapel Hill",
+		"I have taken a special interest in development processes that leverage distributed systems and computer science fundamentals",
+		"Projects that I typically enjoy include developing code to automate Kubernetes deployments resulting in highly-available SaaS offerings",
+		"When I’m not programming, I enjoy weightlifting, playing basketball, and cooking",
+	],
 }
 
 export const work = {
-  title: "What I do",
-  cards: [
-    {
-      title: "Mobile App Development",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
-      icons: null,
-    },
-    {
-      title: "Web Development",
-      description: "I create responsive static websites using Reactjs.",
-      icons: null,
-    }
-  ],
+	title: "What I do",
+	cards: [
+		{
+			title: "Backend Development",
+			description: "I work on backend projects ranging from short-term stock price predictors to modeling and training models for cancer detection",
+			icons: null,
+		},
+		{
+			title: "Numerical Methods",
+			description: "I use differential equations, physics theory, and statistics to model real-world problems",
+			icons: null,
+		},
+		{
+			title: "Infrastructure Engineering",
+			description: "I have a strong understanding of Kubernetes and have been a sole or key contributor to three large-scale projects.",
+			icons: null,
+		}
+	],
 }
 
 export const projects = {
-  title: "Projects",
-  cards: [
-    {
-      title: "StarBook",
-      description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
-      icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-        },
-        {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/star_book",
-        },
-      ]
-    },
-    {
-      title: "QuranTalk",
-      description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
-      icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-        },
-        {
-          icon: faGooglePlay,
-          link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
-        },
-      ]
-    },
-    {
-      title: "Portfolio",
-      description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
-      icons: [
-        {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/portfolio",
-        },
-      ]
-    },
-  ],
+	title: "Projects",
+	cards: [
+		{
+			title: "Peer2Peer Lending Insights",
+			description: "Developed a Peer-2-Peer investment strategy employing a precision-maximizing random forest model.",
+			icons: [
+				{
+					icon: faGithub,
+					link: "https://github.com/kunalpa/Peer2Peer_lending_insights",
+				},
+			]
+		},
+		{
+			title: "Covid-19 Effects Dashboard",
+			description: "A dashboard built to track the effects of Covid-19 at a global scale",
+			icons: [
+				{
+					icon: faGithub,
+					link: "https://github.com/comp426-2022-spring/a99-atlas",
+				},
+			]
+		},
+		{
+			title: "Compressible Fluid Dynamics",
+			description: "Solving the Compressible Fluid Equations Numerically",
+			icons: [
+				{
+					icon: faGithub,
+					link: "https://github.com/kunalpa/Compressible-Fluid-Equations",
+				},
+			]
+		}
+	],
 }
 
 export const contact = {
-  title: "Get in touch",
-  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
-  buttons: [
-    {
-      title: "Email Me",
-      link: "mailto:hashirshoaeb@gmail.com",
-      isPrimary: true,
-    },
-    {
-      title: "Schedule Meeting",
-      link: "https://topmate.io/hashirshoaeb",
-      isPrimary: false,
-    },
-  ]
+	title: "Get in touch",
+	description: "If you have any questions, comments, or just want to say hi, feel free to email me!",
+	buttons: [
+		{
+			title: "Email Me",
+			link: "mailto:kunal.pathak@unc.edu",
+			isPrimary: true,
+		}
+	]
 }
 
 // SEARCH ENGINE 
 export const SEO = {
-  // 50 - 60 char  
-  title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-  description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
-  image: profile.src,
+	// 50 - 60 char  
+	title: "Kunal Pathak | Computer Scientist | Software Engineer",
+	description: "I am interested in software engineering.",
+	image: profile.src,
 }
 
 export const links = {
-  image: profile.src,
-  title: "@hashirshoaeb",
-  description: "Computer Engineer | Flutter | Reactjs Developer",
-  cards: [
-    {
-      title: "My website",
-      link: "https://hashirshoaeb.com/",
-    },
-    {
-      title: "QuranTalk App",
-      link: "https://www.qurantalk.app/",
-    },
-    {
-      title: "StarBook App",
-      link: "https://starbook.dev/",
-    },
-    {
-      title: "My GitHub",
-      link: "https://github.com/hashirshoaeb/",
-    },
-    {
-      title: "My LinkedIn",
-      link: "https://www.linkedin.com/in/hashirshoaeb/",
-    },
-  ]
+	image: profile.src,
+	title: "@kunalpa",
+	description: "Software Engineer | Computer Scientist | Physics Enthusiast",
+	cards: [
+		{
+			title: "My website",
+			link: "https://kunalpa.github.io/portfolio/",
+		},
+		{
+			title: "My GitHub",
+			link: "https://github.com/kunalpa/",
+		},
+		{
+			title: "My LinkedIn",
+			link: "https://www.linkedin.com/in/kunalhpathak/",
+		},
+	]
 }
